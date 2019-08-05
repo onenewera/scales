f4468cc4e60d27aa111cc54f997bb4f3b55a6c15
Donny Wilson <heydwilson@gmail.com>
2019-08-05 17:07:15 -0400

    First commit Message

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+// import UnitConverter from './components/unitConverter';
+
 
 function App() {
+
+  // Adjust this for global scale
+  const scaleLevel = 100
+
+  // Scale Numbers
+  let earthScale = 12754000
+  let humanScale = 18.2
+  let ratScale = 2.8ik8,
+  let microbeScale = .00000001
+  let planckScale = .0000024
+
+  // Scale Percentages
+  let earth = earthScale / scaleLevel * 100 + "%"
+  let human = humanScale / scaleLevel * 100 + "%"
+  let rat = ratScale / scaleLevel * 100 + "%"
+  let microbe = microbeScale / scaleLevel * 100 + "%"
+  let planck = planckScale / scaleLevel * 100 + "%"
+
+  // let earthStyles = {
+  //   fontSize: earthScale + 'px',
+  // }
+    let appStyles = {
+      display: 'flex',
+      flexDirection: 'column',
+    }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="App-header" style={appStyles}>
+        <h4 className="type-percentage" id="earth">Earth: {earth}</h4>,
+        <h4 className="type-percentage">Human: {human}</h4>ik,
+        <h4 className="type-percentage">Rat: {rat}</h4>
+        <h4 className="type-percentage">Microbe: {microbe}</h4>
+        <h4 className="type-percentage">Planck: {planck}</h4>
       </header>
     </div>
   );
---
+++ src/components/unitConverter.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+function UnitConverter(unit) {
+    if (unit = "ft") {
+        finalCalculation = 12
+    }
+  let finalCalculation = 2;
+
+  return finalCalculation
+}
+
+export default UnitConverter;
